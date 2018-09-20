1dbd86604bccddb0b02af8bdcca06c56d88b5f67
Alejandro Velásquez Molina <alejandrovelasquezmolina@gmail.com>
2018-09-20 02:39:23 -0500

    Mostrando los resultados en el componente

--- src/components/Clima.js
+++ src/components/Clima.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 class Clima extends Component{
 
-  
   mostrarResultado = () => {
     const {name, weather, main, clouds} = this.props.resultado;
     
@@ -10,6 +9,10 @@ class Clima extends Component{
     // convert kelvin to centigrade
     const kelvin = 273.15;
 
+    // icon weather
+    const urlIcon = `http://openweathermap.org/img/w/${weather[0].icon}.png`;
+
+    const alt = `Clima de ${name}`;
     return(
       <div className="row">
        <div className="resultado col s12 m8 l6 offset-m2 offset-l3">
@@ -18,7 +21,10 @@ class Clima extends Component{
              <h2>Resultado Clima de: {name}</h2>
              <p className="temperatura">
                Actual: { (main.temp - kelvin).toFixed(2) } &deg;C
+               <img src={urlIcon} alt={alt} />
              </p>
+             <p>Max. {main.temp_max - kelvin} &deg;C</p>
+             <p>Min. {main.temp_min - kelvin} &deg;C</p>
            </div>
          </div>
        </div>
